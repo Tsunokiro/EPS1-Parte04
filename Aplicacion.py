8d2e72db09398c6281926e29659c830e62ef3158
MGonzaloC <19130106090@untels.edu.pe>
2022-11-07 10:44:08 -0500

    Base de datos creados por Cabezas

--- Aplicacion.py
+++ Aplicacion.py
@@ -5,3 +5,17 @@ print("2. Eliminar")
 print("3. Editar")
 print("4. Listar")
 print("5. Salir")
+
+import sqlite3
+conexion = sqlite3.connect("Cabezasalmacen.db")
+#idalumno INTEGER PRIMARY KEY AUTOINCREMENT,
+tabla_producto="""CREATE TABLE productos(
+                idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+                codigo TEXT UNIQUE,
+                nombre TEXT UNIQUE,
+                precio REAL      
+                )
+               """
+cursor = conexion.cursor()
+cursor.execute(tabla_producto)
+conexion.close()
